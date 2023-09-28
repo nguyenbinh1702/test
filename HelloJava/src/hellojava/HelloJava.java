86d076250a9e2e81228f9bcf48513eaef2a63d1f
nguyenbinh1702 <nguyenthebinh17022002@gmail.com>
2023-09-28 09:07:36 +0700

    test

---
+++ HelloJava/src/hellojava/HelloJava.java
@@ -0,0 +1,17 @@
+
+package hellojava;
+
+/**
+ *
+ * @author admin
+ */
+public class HelloJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+    
+}
